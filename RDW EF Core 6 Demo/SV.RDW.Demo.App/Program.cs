f506676cb522b2a7e2e4e3c64fb36f515ec6561b
Michiel Doeven <m.doeven@sharevalue.nl>
2022-01-31 20:45:13 +0100

    Initiële commit.

---
+++ RDW EF Core 6 Demo/SV.RDW.Demo.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+using var log = new LoggerConfiguration()
+    .WriteTo.Console(
+        theme: AnsiConsoleTheme.Code,
+        outputTemplate: "[{Timestamp:dd.MM.yyyy HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+Log.Logger = log;
+Log.Information("ShareValue Tech Thursday - 24 februari 2022");
+Log.Information("EF Core 6 Demo");
