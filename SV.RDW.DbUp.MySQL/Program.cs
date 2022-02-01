757b62f40c011cd742b7638a71df36c78310e7d8
Michiel Doeven <m.doeven@sharevalue.nl>
2022-02-01 13:45:25 +0100

    DbUp setup

--- SV.RDW.DbUp.MySQL/Program.cs
+++ SV.RDW.DbUp.MySQL/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+var connectionString = args.FirstOrDefault();
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+	var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+	var builder = new ConfigurationBuilder()
+		.AddJsonFile($"appsettings.json", true, true)
+		.AddJsonFile($"appsettings.{env}.json", true, true);
+
+	var config = builder.Build();
+	connectionString = config["connectionString"];
+}
+	
+
+var upgrader = DeployChanges.To
+				.MySqlDatabase(connectionString)
+				.WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+				.LogToConsole().LogScriptOutput()
+				.Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+	Console.ForegroundColor = ConsoleColor.Red;
+	Console.WriteLine(result.Error);
+	Console.ResetColor();
+#if DEBUG
+	Console.ReadLine();
+#endif
+	return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("DbUp succesvol afgerond!");
+Console.ResetColor();
+
+return 0;
