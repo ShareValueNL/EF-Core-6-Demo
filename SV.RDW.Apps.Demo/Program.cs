3596a71fc3b7c99a06343cc9a741137a4acd6685
Michiel Doeven <m.doeven@sharevalue.nl>
2022-02-18 19:10:42 +0100

    WIP

--- SV.RDW.Apps.Demo/Program.cs
+++ SV.RDW.Apps.Demo/Program.cs
@@ -5,6 +5,7 @@ using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
 using SV.RDW.Migrations.MySQL;
 using SV.RDW.Migrations.PostgreSQL;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
 
 using var log = new LoggerConfiguration()
     .WriteTo.Console(
@@ -25,7 +26,25 @@ string connectionStringMySql = config["ConnectionStrings:mysql"];
 var serviceProvider = new ServiceCollection()
            .AddNpgsql<PostgreSQLContext>(connectionStringPostgres)
            .AddDbContext<MySQLContext>(options =>
-		   {
-               options.UseMySql(ServerVersion.AutoDetect(connectionStringMySql));
-		   })
+           {
+               options.EnableDetailedErrors(true);
+               options.UseMySql(connectionStringMySql, ServerVersion.AutoDetect(connectionStringMySql));
+           })
            .BuildServiceProvider();
+
+var pgContext = serviceProvider.GetRequiredService<PostgreSQLContext>();
+pgContext.Merken.Add(new SV.RDW.Data.Entities.Merk
+{
+    Id = 2,
+    Naam = "Volvo"
+});
+await pgContext.SaveChangesAsync();
+
+var myContext = serviceProvider.GetRequiredService<MySQLContext>();
+myContext.Merken.Add(new SV.RDW.Data.Entities.Merk
+{
+    Id = 2,
+    Naam = "Volvo"
+});
+await myContext.SaveChangesAsync();
+Log.Information("Done");
--- SV.RDW.Apps.Import/ImportFunction.cs
+++ SV.RDW.Apps.Import/ImportFunction.cs
@@ -1,7 +1,11 @@
 using Microsoft.Azure.WebJobs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SV.RDW.Data.Entities.ImportJson;
 using SV.RDW.Migrations.MySQL;
+using SV.RDW.Migrations.PostgreSQL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,20 +17,29 @@ namespace SV.RDW.Apps.Import
 {
     public class ImportFunction
     {
-        private readonly MySQLContext _context;
 
         // Dit moeten we ergens bij gaan houden waar we gebleven waren
         private DateTime _firstAdmissionDate = new DateTime(2022, 1, 3);
-
-        public ImportFunction(MySQLContext context)
-        {
-            _context = context;
-        }
 
         [FunctionName("ImportFunction")]
         public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"Timer trigger function executed at: {DateTime.Now}");
+
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true);
+            var config = builder.Build();
+            string connectionStringPostgres = config["ConnectionStrings:Postgres"];
+            string connectionStringMySql = config["ConnectionStrings:mysql"];
+
+
+            var serviceProvider = new ServiceCollection()
+                       .AddNpgsql<PostgreSQLContext>(connectionStringPostgres)
+                       .AddDbContext<MySQLContext>(options =>
+                       {
+                           options.UseMySql(ServerVersion.AutoDetect(connectionStringMySql));
+                       })
+                       .BuildServiceProvider();
+
 
             var vehicles = await new RdwHttpClient().GetVehicles(_firstAdmissionDate);
 
