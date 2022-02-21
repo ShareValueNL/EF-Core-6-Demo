512bfa06908cf1beb7a4b32f083c344498d21436
Michiel Doeven <m.doeven@sharevalue.nl>
2022-02-21 22:24:27 +0100

    Toppers class geïmplementeerd

--- SV.RDW.Apps.Demo/Toppers.cs
+++ SV.RDW.Apps.Demo/Toppers.cs
@@ -1,4 +1,7 @@
-﻿using Serilog;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Serilog;
+using SV.RDW.Data.Entities;
 using SV.RDW.Migrations.MySQL;
 using SV.RDW.Migrations.PostgreSQL;
 
@@ -6,18 +9,93 @@ namespace SV.RDW.Apps.Demo;
 
 internal class Toppers : BaseQuery
 {
+    private IIncludableQueryable<Voertuig, VoertuigSoort?> _pgVoertuigenMetSoorten;
+    private IIncludableQueryable<Voertuig, VoertuigSoort?> _myVoertuigenMetSoorten;
+
+    private IIncludableQueryable<Voertuig, Merk?> _pgVoertuigenMetMerk; 
+    private IIncludableQueryable<Voertuig, Merk?> _myVoertuigenMetMerk;
+
     public Toppers(MySQLContext mySQLContext, PostgreSQLContext postgreSQLContext) : base(mySQLContext, postgreSQLContext)
     {
+        _pgVoertuigenMetSoorten = _postgreSQLContext.Voertuigen.Include(x => x.VoertuigSoort);
+        _myVoertuigenMetSoorten = _mySQLContext.Voertuigen.Include(x => x.VoertuigSoort);
+
+        _pgVoertuigenMetMerk = _postgreSQLContext.Voertuigen.Include(x => x.Merk);
+        _myVoertuigenMetMerk = _mySQLContext.Voertuigen.Include(x => x.Merk);
     }
 
     internal void PerSoort()
     {
-        throw new NotImplementedException();
+        var pgNu = DateTime.Now;
+        var pgSoorten = _pgVoertuigenMetSoorten.GroupBy(x => x.VoertuigSoort.Naam)
+                 .Select(g => new { g.Key, Count = g.Count() })
+                 .OrderByDescending(x => x.Count);
+        var pgDict = new Dictionary<string, int>();
+        foreach (var item in pgSoorten)
+        {
+            pgDict.Add(item.Key, item.Count);
+        }
+        var pgTijd = DateTime.Now.Subtract(pgNu).TotalMilliseconds;
+
+        var myNu = DateTime.Now;
+        var mySoorten = _myVoertuigenMetSoorten.GroupBy(x => x.VoertuigSoort.Naam)
+                 .Select(g => new { g.Key, Count = g.Count() })
+                 .OrderByDescending(x => x.Count);
+        var myDict = new Dictionary<string, int>();
+        foreach (var item in mySoorten)
+        {
+            myDict.Add(item.Key, item.Count);   
+        }
+        var myTijd = DateTime.Now.Subtract(myNu).TotalMilliseconds;
+
+        var msg = "\n| Voertuigsoort                            | Aantal | Aantal |";
+
+        foreach (string key in pgDict.Keys)
+        {
+            msg += $"\n| {key,-40} | {pgDict[key], 6} | {myDict[key], 6} |";
+        }
+        
+        msg += $"\n\n| Tijd                                     | {pgTijd,6:0.0} | {myTijd,6:0.0} |";
+
+        Log.Information(msg);
     }
 
     internal void Automerken()
     {
-        throw new NotImplementedException();
+        var pgNu = DateTime.Now;
+        var pgSoorten = _pgVoertuigenMetMerk.GroupBy(x => x.Merk.Naam)
+                 .Select(g => new { g.Key, Count = g.Count() })
+                 .OrderByDescending(x => x.Count)
+                 .Take(25);
+        var pgDict = new Dictionary<string, int>();
+        foreach (var item in pgSoorten)
+        {
+            pgDict.Add(item.Key, item.Count);
+        }
+        var pgTijd = DateTime.Now.Subtract(pgNu).TotalMilliseconds;
+
+        var myNu = DateTime.Now;
+        var mySoorten = _myVoertuigenMetMerk.GroupBy(x => x.Merk.Naam)
+                 .Select(g => new { g.Key, Count = g.Count() })
+                 .OrderByDescending(x => x.Count)
+                 .Take(25);
+        var myDict = new Dictionary<string, int>();
+        foreach (var item in mySoorten)
+        {
+            myDict.Add(item.Key, item.Count);
+        }
+        var myTijd = DateTime.Now.Subtract(myNu).TotalMilliseconds;
+
+        var msg = "\n| Merk                 | Aantal | Aantal |";
+
+        foreach (string key in pgDict.Keys)
+        {
+            msg += $"\n| {key,-20} | {pgDict[key],6} | {myDict[key],6} |";
+        }
+
+        msg += $"\n\n| Tijd                 | {pgTijd,6:0.0} | {myTijd,6:0.0} |";
+
+        Log.Information(msg);
     }
 }
 
